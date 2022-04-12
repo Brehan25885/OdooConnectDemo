89a02667f35d2ae0c2537b2f8c1db052ad5847c6
brehan.mohamed <brehan.mohamed@objects.ws>
2022-04-12 15:36:53 +0200

    change port

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 app="docker.flask"
 docker build -t ${app} .
-docker run -d -p 56733:80 \
+docker run -d -p 3030:80 \
   --name=${app} \
   --network=objects-ws_default\
   -v $PWD:/app ${app}
